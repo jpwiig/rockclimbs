7926ca1872ae09c3503205092542ff3d155987fe
jpwiig <jpwiig@gmail.com>
2022-11-12 22:20:44 +0100

    innit

---
+++ src/main/java/com/example/rockclimbs/RockclimbsApplication.java
@@ -0,0 +1,13 @@
+package com.example.rockclimbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockclimbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RockclimbsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rockclimbs/RockclimbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rockclimbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockclimbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
