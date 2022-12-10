c602c8042e52d572661c7c59740e98a9a64e2c2b
jpwiig <jpwiig@gmail.com>
2022-12-10 19:54:03 +0100

    smthng

--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -1,20 +1,21 @@
 
+let list;
 
-function getRoutes(chckroute,list) {
-
-    for(let i = 0; i<list.length; i++){
-        if (list[i] == chckroute) return true;
-    }
-
-//ajax call for the routes avaible
-}
 
 $(()=>({
     $:("#searchclk").click(() => {
         alert("a")
-        getRoutes(document.getElementById("search-input"), $.get("api/test/", () => {
-        }));
+        $.get("/api/test/", (list) => {
+            getRoutes(list);
+        })
 
     })
 }))
+function getRoutes(list) {
 
+    for(let i = 0; i<list.length; i++){
+
+    }
+    $("#results").append();
+//ajax call for the routes avaible
+}
