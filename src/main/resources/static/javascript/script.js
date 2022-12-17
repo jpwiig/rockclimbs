68600923d09d33c5db19ea87cafb366e448f9dcb
jpwiig <jpwiig@gmail.com>
2022-12-17 21:33:24 +0100

    love that javascript

--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -3,19 +3,26 @@ let list;
 
 
 $(()=>({
-    $:("#searchclk").click(() => {
-        alert("a")
+   /* $("#searchclk").click(() => {
+        console.log("Lættis")
         $.get("/api/test/", (list) => {
             getRoutes(list);
         })
 
-    })
+    })*/
+
+    $("#ListOfPlaces").click(
+        $.get("/api/test/", (list) => {
+            getRoutes(list)
+        }))
+
 }))
 function getRoutes(list) {
 
     for(let i = 0; i<list.length; i++){
         $("#results").append(list[i]);
         console.log(list);
+        console.log("hei")
     }
 //ajax call for the routes avaible
 }
