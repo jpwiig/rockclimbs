22ccc81d55121b0f3e5554ae3ae50610939f5761
jpwiig <jpwiig@gmail.com>
2022-12-22 19:41:55 +0100

    exploring new ideas

--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -2,27 +2,41 @@
 let list;
 
 
-$(()=>({
-   /* $("#searchclk").click(() => {
-        console.log("Lættis")
-        $.get("/api/test/", (list) => {
-            getRoutes(list);
-        })
+$(()=> {
+    /* $("#searchclk").click(() => {
+         console.log("Lættis")
+         $.get("/api/test/", (list) => {
+             getRoutes(list);
+         })
 
-    })*/
+     })*/
 
     $("#ListOfPlaces").click(
-        $.get("/api/test/", (list) => {
+      /*  $.get("/api/test/", (list) => {
             getRoutes(list)
-        }))
+        })*/
+    console.log("funk")
 
-}))
+    )
+})
+
+
 function getRoutes(list) {
 
     for(let i = 0; i<list.length; i++){
         $("#results").append(list[i]);
         console.log(list);
         console.log("hei")
+    }
+    let grade;
+    function TypeofGrade(grade){
+
+        //get logic on place, then create in db
+        let gradestype = {
+            French : ["4a", "4b", "4c", "5a", "5b", "5c" ],
+            UIAA : ["IV", "IV+", "V", "V+"],
+            Yosemite : ["5.5","5.6","5.7", "5.8"]
+        }
     }
 //ajax call for the routes avaible
 }
