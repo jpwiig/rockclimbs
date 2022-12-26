fc8d73e3c6331ce3d72929b34caf9792dc525a70
jpwiig <jpwiig@gmail.com>
2022-12-26 13:23:01 +0100

    working on inproving my JS

--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -1,8 +1,7 @@
-
 let list;
 
 
-$(()=> {
+$(() => {
     /* $("#searchclk").click(() => {
          console.log("Lættis")
          $.get("/api/test/", (list) => {
@@ -12,34 +11,41 @@ $(()=> {
      })*/
 
     $("#ListOfPlaces").click(
-      /*  $.get("/api/test/", (list) => {
-            getRoutes(list)
-        })*/
-    console.log("funk")
-
+        /*  $.get("/api/test/", (list) => {
+              getRoutes(list)
+          })*/
+        console.log("funk")
     )
 })
 
 
 function getRoutes(list) {
 
-    for(let i = 0; i<list.length; i++){
+    for (let i = 0; i < list.length; i++) {
         $("#results").append(list[i]);
         console.log(list);
         console.log("hei")
     }
     let grade;
-    function TypeofGrade(grade){
+    let out;
+
+    function TypeofGrade(grade) {
 
         //get logic on place, then create in db
         let gradestype = [
-            { French : ["4a", "4b", "4c", "5a", "5b", "5c" ]},
+            {French: ["4a", "4b", "4c", "5a", "5b", "5c"]},
             {UIAA: ["IV", "IV+", "V", "V+"]},
             {Yosemite: ["5.5", "5.6", "5.7", "5.8"]}
         ]
-        if (grade === gradestype){
-            gradestype.forEach()
+        for (let i in gradestype) {
+            if (i === grade) {
+                out += "<option>" + gradestype.at(gradestype.indexOf(grade)) + "</option>";
+            }
         }
+        let grades = document.getElementById("gradeName");
+        grades.style.visibility = 'visible'
+        grades.append(out);
     }
+
 //ajax call for the routes avaible
 }
